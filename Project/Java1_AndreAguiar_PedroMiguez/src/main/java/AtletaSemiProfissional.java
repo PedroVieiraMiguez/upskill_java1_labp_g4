4595a406c4ed0a50788b7dd499a8adcf9d3be4e9
Pedro Miguez <pedrovieiramiguez@upskill.pt>
2020-11-25 10:38:45 +0000

    Update AtletaSemiProfissional.java

--- Project/Java1_AndreAguiar_PedroMiguez/src/main/java/AtletaSemiProfissional.java
+++ Project/Java1_AndreAguiar_PedroMiguez/src/main/java/AtletaSemiProfissional.java
@@ -1,2 +1,30 @@
-public class AtletaSemiProfissional {
+public class AtletaSemiProfissional extends AtletaNaoProfissional {
+
+    private static double parcelaFixa = 100;
+
+
+    public AtletaSemiProfissional(String nome, int numIdCivil, int idade, double freqCardRepouso, double premioAcumulado,
+                        Genero genero, Atividade atividade, ObjectivoTreino objectivo, int antiguidade) {
+        super(nome, numIdCivil, idade, freqCardRepouso, premioAcumulado, genero, atividade, objectivo, antiguidade );
+    }
+
+    public AtletaSemiProfissional(){
+        super();
+    }
+
+    public static double getParcelaFixa() {
+        return parcelaFixa;
+    }
+
+    public static void setParcelaFixa(double parcelaFixa) {
+        AtletaSemiProfissional.parcelaFixa = parcelaFixa;
+    }
+
+    public double determinarParcela1() {
+        return 0;
+    }
+
+    public double determinarParcela2() {
+        return 0;
+    }
 }
