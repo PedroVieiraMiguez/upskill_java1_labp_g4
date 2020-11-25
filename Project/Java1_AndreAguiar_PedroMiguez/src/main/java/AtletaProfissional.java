e38bbae74adf90a17e405937bb6aa4988bb80e2e
Andre Aguiar <andrefaguiar@upskill.pt>
2020-11-25 12:22:36 +0000

    method parcela2 in atletaProf

--- Project/Java1_AndreAguiar_PedroMiguez/src/main/java/AtletaProfissional.java
+++ Project/Java1_AndreAguiar_PedroMiguez/src/main/java/AtletaProfissional.java
@@ -55,7 +55,7 @@ public class AtletaProfissional extends Atleta{
 
     @Override
     public double determinarParcela2() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return getPremioAcumulado() * percValorVar;
     }
     
     
