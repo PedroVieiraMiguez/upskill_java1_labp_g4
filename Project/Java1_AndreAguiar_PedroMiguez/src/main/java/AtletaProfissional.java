e1fb6054c4890a8ed636cf1998274d2ad9e84e4a
Andre Aguiar <andrefaguiar@upskill.pt>
2020-11-25 10:58:21 +0000

    Update AtletaProfissional.java

--- Project/Java1_AndreAguiar_PedroMiguez/src/main/java/AtletaProfissional.java
+++ Project/Java1_AndreAguiar_PedroMiguez/src/main/java/AtletaProfissional.java
@@ -1,2 +1,63 @@
-public class AtletaProfissional {
+public class AtletaProfissional extends Atleta{
+    
+    private double valorFixo;
+    
+    private static final double VALOR_FIXO_OMISSAO = 0;
+    
+    private static double percValorVar = 20;
+    
+    
+    public AtletaProfissional(String nome, int numIdCivil, int idade, double freqCardRepouso, double premioAcumulado,
+            Genero genero, Atividade atividade, ObjectivoTreino objectivo, double valorFixo){
+        super(nome, numIdCivil, idade, freqCardRepouso, premioAcumulado, genero, atividade, objectivo);
+        setValorFixo(valorFixo);
+    }
+    
+    
+    public AtletaProfissional(){
+        super();
+        setValorFixo(VALOR_FIXO_OMISSAO);
+    }
+    
+
+    /**
+     * @return the valorFixo
+     */
+    public double getValorFixo() {
+        return this.valorFixo;
+    }
+
+    /**
+     * @param valorFixo the valorFixo to set
+     */
+    public void setValorFixo(double valorFixo) {
+        this.valorFixo = valorFixo;
+    }
+
+    /**
+     * @return the percValorVar
+     */
+    public static double getPercValorVar() {
+        return percValorVar;
+    }
+
+    /**
+     * @param aPercValorVar the percValorVar to set
+     */
+    public static void setPercValorVar(double aPercValorVar) {
+        percValorVar = aPercValorVar;
+    }
+
+    @Override
+    public double determinarParcela1() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double determinarParcela2() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
 }
