23a27be0392492fff248d6c5a6a16ed8748b3396
Pedro Miguez <pedrovieiramiguez@upskill.pt>
2020-11-25 11:08:30 +0000

    Update AtletaTest.java

--- Project/Java1_AndreAguiar_PedroMiguez/src/test/java/AtletaTest.java
+++ Project/Java1_AndreAguiar_PedroMiguez/src/test/java/AtletaTest.java
@@ -18,18 +18,40 @@ class AtletaTest {
     void testCalcularPagamento() {
         AtletaAmador a1 = new AtletaAmador("João", 123, 20, 60, 100,
                 Genero.MASCULINO, Atividade.CAMINHADA, ObjectivoTreino.QUEIMA_GORDURA, 21);
-        double expected = 
+        double expected = 100 * 0.2 + 100 * 0.07;
+        double result = a1.calcularPagamento();
+
+        assertEquals(expected, result);
     }
 
     @Test
     void testFreqCardTrabalho() {
+        AtletaAmador a1 = new AtletaAmador("João", 123, 20, 60, 100,
+                Genero.MASCULINO, Atividade.CAMINHADA, ObjectivoTreino.QUEIMA_GORDURA, 21);
+        double expected = 60 + (0.75 * ((208.75 - (0.73 * 20)) - 60));
+        double result = a1.freqCardTrabalho();
+
+        assertEquals(expected, result);
     }
 
     @Test
     void testFreqCardMax() {
+        AtletaAmador a1 = new AtletaAmador("João", 123, 20, 60, 100,
+                Genero.MASCULINO, Atividade.CAMINHADA, ObjectivoTreino.QUEIMA_GORDURA, 21);
+        double expected = 208.75 - (0.73 * 20);
+        double result = a1.freqCardMax();
+
+        assertEquals(expected, result);
     }
 
     @Test
     void testDeterminarIT() {
+        AtletaAmador a1 = new AtletaAmador("João", 123, 20, 60, 100,
+                Genero.MASCULINO, Atividade.CAMINHADA, ObjectivoTreino.QUEIMA_GORDURA, 21);
+        double expected = 0.75;
+        double result = a1.determinarIT();
+
+        assertEquals(expected, result);
+
     }
 }
