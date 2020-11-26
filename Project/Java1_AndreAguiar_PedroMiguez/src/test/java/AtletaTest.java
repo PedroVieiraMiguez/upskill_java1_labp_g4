652811a12bf50cc600c94f46d3eb60617ca3cde7
Andre Aguiar <andrefaguiar@upskill.pt>
2020-11-26 10:38:22 +0000

    updates Test classes to adress test errors

--- Project/Java1_AndreAguiar_PedroMiguez/src/test/java/AtletaTest.java
+++ Project/Java1_AndreAguiar_PedroMiguez/src/test/java/AtletaTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class AtletaTest {
 
     @Test
-    void testAddPremio() {
+    public void testAddPremio() {
         AtletaAmador a1 = new AtletaAmador();
         a1.addPremio(50);
         int expected = 50;
@@ -17,63 +17,63 @@ class AtletaTest {
     }
 
     @Test
-    void testCalcularPagamentoAmador() {
-        AtletaAmador a1 = new AtletaAmador("João", 123, 20, 60, 100,
+    public void testCalcularPagamentoAmador() {
+        AtletaAmador a1 = new AtletaAmador("Joana", 123, 20, 60, 100,
                 Genero.MASCULINO, Atividade.CAMINHADA, ObjectivoTreino.QUEIMA_GORDURA, 21);
         double expected = 100 * 0.2 + 100 * 0.07;
         double result = a1.calcularPagamento();
 
-        assertEquals(expected, result);
+        assertEquals(expected, result, 0.0);
     }
 
     @Test
-    void testCalcularPagamentoProfissional() {
-        AtletaProfissional a1 = new AtletaProfissional("João", 123, 20, 60, 100,
+    public void testCalcularPagamentoProfissional() {
+        AtletaProfissional a1 = new AtletaProfissional("Joana", 123, 20, 60, 100,
                 Genero.MASCULINO, Atividade.CAMINHADA, ObjectivoTreino.QUEIMA_GORDURA, 100);
         double expected = 100 + (100 * 0.2);
         double result = a1.calcularPagamento();
 
-        assertEquals(expected, result);
+        assertEquals(expected, result, 0.0);
     }
 
     @Test
-    void testCalcularPagamentoSemiProfissional() {
-        AtletaSemiProfissional a1 = new AtletaSemiProfissional("João", 123, 20, 60, 100,
+    public void testCalcularPagamentoSemiProfissional() {
+        AtletaSemiProfissional a1 = new AtletaSemiProfissional("Joana", 123, 20, 60, 100,
                 Genero.MASCULINO, Atividade.CAMINHADA, ObjectivoTreino.QUEIMA_GORDURA, 15);
         double expected = 100+ (100 * 0.08);
         double result = a1.calcularPagamento();
 
-        assertEquals(expected, result);
+        assertEquals(expected, result, 0.0);
     }
 
     @Test
-    void testFreqCardTrabalho() {
-        AtletaAmador a1 = new AtletaAmador("João", 123, 20, 60, 100,
+    public void testFreqCardTrabalho() {
+        AtletaAmador a1 = new AtletaAmador("Joana", 123, 20, 60, 100,
                 Genero.MASCULINO, Atividade.CAMINHADA, ObjectivoTreino.QUEIMA_GORDURA, 21);
         double expected = 60 + (0.6 * ((208.75 - (0.73 * 20)) - 60));
         double result = a1.freqCardTrabalho();
 
-        assertEquals(expected, result);
+        assertEquals(expected, result, 0.0);
     }
 
     @Test
-    void testFreqCardMax() {
-        AtletaAmador a1 = new AtletaAmador("João", 123, 20, 60, 100,
+    public void testFreqCardMax() {
+        AtletaAmador a1 = new AtletaAmador("Joana", 123, 20, 60, 100,
                 Genero.MASCULINO, Atividade.CAMINHADA, ObjectivoTreino.QUEIMA_GORDURA, 21);
         double expected = 208.75 - (0.73 * 20);
         double result = a1.freqCardMax();
 
-        assertEquals(expected, result);
+        assertEquals(expected, result, 0.0);
     }
 
     @Test
-    void testDeterminarIT() {
-        AtletaAmador a1 = new AtletaAmador("João", 123, 20, 60, 100,
+    public void testDeterminarIT() {
+        AtletaAmador a1 = new AtletaAmador("Joana", 123, 20, 60, 100,
                 Genero.MASCULINO, Atividade.CAMINHADA, ObjectivoTreino.QUEIMA_GORDURA, 21);
         double expected = 0.6;
         double result = a1.determinarIT();
 
-        assertEquals(expected, result);
+        assertEquals(expected, result, 0.0);
 
     }
 }
